5f260fae4970a13f1dd05c69188e2cb11bd71d8f
Daniel Chang <daniel.changk@gmail.com>
2022-07-04 11:59:11 -0500

    implemented core ticket routes and tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,7 @@
 import request from "supertest";
+
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -60,12 +62,22 @@ it("returns an error if an invalid price is provided", async () => {
 
 it("creates a ticket with valid inputs", async () => {
   // add in a check to make sure ticket was saved
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = "asldkfj";
 
   await request(app)
     .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({
-      title: "asldkfj",
+      title,
       price: 20,
     })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(20);
 });
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -33,7 +33,7 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload. { id, email }
   const payload = {
-    id: "123123jkjei231",
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
