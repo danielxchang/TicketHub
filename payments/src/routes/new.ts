0ce921c2d3426dd95b32f18a0d4e2769aed11fac
Daniel Chang <daniel.changk@gmail.com>
2022-07-13 13:33:47 -0500

    implement stripe payment processing code and wired up payment created event processing

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,10 @@ import {
 } from "@tickethub-dc/common";
 
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from "../events/publishers/payment-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -35,7 +39,25 @@ router.post(
       throw new BadRequestError("Cannot pay for a cancelled order");
     }
 
-    res.send({ success: true });
+    const charge = await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token,
+    });
+
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
+
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.status(201).send({ id: payment.id });
   }
 );
 
