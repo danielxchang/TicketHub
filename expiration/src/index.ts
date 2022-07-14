3128752d1e778cdaaa0832793203e6c3654d3b32
Daniel Chang <daniel.changk@gmail.com>
2022-07-14 14:14:16 -0500

    new startup messages

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
+  console.log("Starting...");
   if (!process.env.NATS_URL) {
     throw new Error("NATS_URL must be defined");
   }
