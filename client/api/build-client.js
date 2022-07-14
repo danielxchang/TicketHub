aa715694b5340c553645c0a49978565fe2cb7747
Daniel Chang <daniel.changk@gmail.com>
2022-07-14 14:59:08 -0500

    updates to fix ingress ngnix and domain name related bugs

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // on the server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.tickethub.lol",
       headers: req.headers,
     });
   } else {
